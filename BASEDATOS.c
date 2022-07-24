87698abd170baed1b2fd8b3c38378e61b67b3887
Snla200 <71408753+Snla200@users.noreply.github.com>
2022-07-24 14:30:00 -0300

    Create BASEDATOS.c

---
+++ BASEDATOS.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char vecPat[8][8];
+                                         
+    int vecDni[8];
+    
+    char vecNom[8][30];
+    
+    
+}BBDD;      //crea el struct llamado BBDD donde se almacenaran los datos de cada vehiculo
+
+
+BBDD vehiculo;  //asigna el sinonimo "vehiculo" a BBDD
+
+
+
