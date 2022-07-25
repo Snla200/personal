f88b458f3098aa1c5850446a7b1206bdca885d40
Snla200 <71408753+Snla200@users.noreply.github.com>
2022-07-25 17:08:03 -0300

    Update patente.c

--- patente.c
+++ patente.c
@@ -1,5 +1,5 @@
-#include "BASEDATOS.c"  //incluya la base de datos
-#include "func.c"
+#include "BASEDATOS.h"  //incluya la base de datos
+#include "func.h"
 
 #define CARACTERES 8
 
@@ -14,21 +14,18 @@ int main(void){
     
     printf("INGRESE PATENTE (TIPO AB123CD) A BUSCAR: ");
     fgets(PATENTE, CARACTERES, stdin);  //ingresa la patente
-   
-    for(int i=0;i<CARACTERES;i++){
-        
-        printf("coductor[%d]\npatente:[%s]\nnombre:[%s]\nDNI:[%s]\n",i,conductor[i].vecPat[i][1,conductor[i].vecNom[i][1],conductor[i].vecDni[i]);
-    
-    }
  
     printf("PATENTE INGRESADA:[%s]\n",p);   //impŕime la patente indicada en el puntero
  */
     
-    asignarBBDD(struct BBDD V);
+   asignarBBDD(vehiculo);
+   
+
     printf("En BBDD:\n");
     printf("NOMBRE DEL CONDUCTOR:[%s]\n",vehiculo.vecNom[0]);
     printf("DNI DEL CONDUCTOR:[%d]\n",vehiculo.vecDni[0]);
     printf("PATENTE DEL VEHICULO:[%s]\n",vehiculo.vecPat[0]);
+    
     return 0;
 }
 
