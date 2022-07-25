79d90d99c8c93d4e259563f4a0e159c127bfe48a
Snla200 <71408753+Snla200@users.noreply.github.com>
2022-07-25 17:09:20 -0300

    Update and rename func.c to func.h

--- func.c
+++
@@ -1,6 +0,0 @@
-void asignarBBDD(struct BBDD V){
-    
-    strcpy(V.vecPat[0],"AB123CD");
-
-    return;
-}
---
+++ func.h
@@ -0,0 +1,16 @@
+void asignarBBDD(struct vehiculo V);
+
+void asignarBBDD(struct vehiculo V){
+
+//   /**/PATENTE                             /**/   DNI                 /**/    NOMBRE 
+    /**/strcpy(vehiculo.vecPat[0],"AB123CD");/**/V.vecDni[0]=111;/**/strcpy(V.vecNom[0],"Juan Carlos");
+    
+    
+    return;
+}
+
+/*
+//   PATENTE                                DNI                     NOMBRE 
+    strcpy(vehiculo.vecPat[0],"AB123CD");vehiculo.vecDni[0]=111;strcpy(vehiculo.vecNom[0],"Juan Carlos");
+    
+    */
